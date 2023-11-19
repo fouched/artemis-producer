b07e6104d0ea0c9b23bf430fbf4802f9db2cc3a2
Fouche du Preez <fouche@limehouse.co.za>
2023-11-19 18:41:00 +0200

    init

---
+++ src/test/java/za/co/limehouse/artemis/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.limehouse.artemis.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
